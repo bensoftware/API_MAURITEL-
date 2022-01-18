1321c190df286717840605ff8046829bfa770066
KingBenD <benjudicaelle@gmail.com>
2022-01-18 16:10:18 +0000

    update

--- src/main/java/com/bpm/apimauritel/serviceImpl/TraitementServiceImpl.java
+++ src/main/java/com/bpm/apimauritel/serviceImpl/TraitementServiceImpl.java
@@ -1,12 +1,10 @@
 package com.bpm.apimauritel.serviceImpl;
 
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.bpm.apimauritel.helpers.MauritelApiHelper;
 import com.bpm.apimauritel.services.GlobalService;
 import com.bpm.apimauritel.services.SmsService;
@@ -16,18 +14,18 @@ import com.bpm.apimauritel.services.TraitementService;
 public class TraitementServiceImpl implements TraitementService{
 
 	@Autowired
-	GlobalService globalService;
+	private GlobalService globalService;
 	
 	@Autowired
-	SmsService smsService;
+	private SmsService smsService;
 
-    final Logger logger = LoggerFactory.getLogger(TraitementServiceImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(TraitementServiceImpl.class);
 
 	@Override
 	public void traitementException() {
 		
 		Date dateEx= globalService.getDateException();
-		int detail= globalService.getDelaiException();
+		int detail =  globalService.getDelaiException();
 		
 		if(dateEx!=null) {
 			boolean verif=MauritelApiHelper.isDetailException(dateEx, detail);
@@ -36,10 +34,10 @@ public class TraitementServiceImpl implements TraitementService{
 				Date dateNotification=globalService.getNotification();
 				
 				if(dateNotification==null) {
-					// envoyer une notif
-					logger.info("SOMELEC API - Exception reponse BD somelec date debut de l exception "+dateEx);
+					// Envoyer une notif
+					logger.info("MAURITEL API - Exception reponse BD MAURITEL date debut de l exception : " + dateEx);
 					try {
-						smsService.sendSms("SOMELEC API - Exception reponse BD somelec date debut de l exception  "+dateEx);
+						smsService.sendSms("MAURITEL API - Exception reponse BD MAURITEL date debut de l exception : " + dateEx);
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -47,16 +45,16 @@ public class TraitementServiceImpl implements TraitementService{
 					globalService.setNotification(new Date());
 				}
 				else {
-					// relance notification
+					// Relance notification
 					int detailRetry= globalService.getDelaiRetryException();
 					
 					boolean verifRetry=MauritelApiHelper.isDetailException(dateNotification, detailRetry);
                     
 					if(verifRetry) {
-						// envoyer une notif
-						logger.info("SOMELEC API - Rappel, Exception reponse BD somelec date debut de l exception "+dateEx);
+						// Envoyer une notif
+						logger.info("MAURITEL API - Rappel, Exception reponse BD MAURITEL date debut de l exception "+dateEx);
 						try {
-							smsService.sendSms("SOMELEC API - Rappel, Exception reponse BD somelec date debut de l exception "+dateEx);
+							smsService.sendSms("MAURITEL API - Rappel, Exception reponse BD MAURITEL date debut de l exception "+dateEx);
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
@@ -64,12 +62,8 @@ public class TraitementServiceImpl implements TraitementService{
 						globalService.setNotification(new Date());
 					}
 				}
-			}
-		
-			
+			}	
 		}
-		
-		
 	}
 
 
@@ -81,24 +75,17 @@ public class TraitementServiceImpl implements TraitementService{
 			globalService.setDateException(new Date());
 			logger.info("Response Exception MOOV-Mauritel "+new Date());
 		}
-		
 	}
-
 
 	@Override
 	public void responseSuccess() {
-		
 		Date dateEx= globalService.getDateException();
 		if(dateEx!=null) {
 			globalService.setDateException(null);
 			globalService.setNotification(null);
 			logger.info("Response Success MOOV-Mauritel "+new Date());
 		}
-			
-		
 	}
 	
-
-
 
 }
